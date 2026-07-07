e2396db83dc6a7269412de1556520198c958a1f5
Hibi25 <128791895+hibiki20@users.noreply.github.com>
2026-07-07 15:10:54 +0900

    Firebaseでのクラウド保存対応

---
+++ sw.js
@@ -0,0 +1,32 @@
+var CACHE = "mitsumori-v3";
+var FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e) {
+  // Firebaseなど外部との通信・データ送信には割り込まない（GET かつ 自サイト内のみ扱う）
+  if (e.request.method !== "GET") return;
+  var url = new URL(e.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  // ネットワーク優先：最新版を取得し、成功したらキャッシュも更新。圏外時のみキャッシュ表示
+  e.respondWith(
+    fetch(e.request).then(function(res) {
+      var copy = res.clone();
+      caches.open(CACHE).then(function(c) { c.put(e.request, copy); });
+      return res;
+    }).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
